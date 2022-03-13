bb7912d68a4385e0ae1f032470051cdb327311a4
Kev Comedia <kevcomedia@gmail.com>
2022-03-13 23:14:56 +0800

    Implement timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Timer from './components/Timer'
 import TimerControl from './components/TimerControl'
 
 const App = () => {
   const [isTimerRunning, setTimerRunning] = useState(false)
+  const [timerType, setTimerType] = useState('Session')
   const [timeLeft, setTimeLeft] = useState(25 * 60 /* seconds */)
   const [breakLength, setBreakLength] = useState(5 /* minutes */)
   const [sessionLength, setSessionLength] = useState(25 /* minutes */)
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      if (timerType === 'Session') {
+        setTimeLeft(breakLength * 60)
+        setTimerType('Break')
+      } else {
+        setTimeLeft(sessionLength * 60)
+        setTimerType('Session')
+      }
+    }
+  }, [timeLeft, timerType, breakLength, sessionLength])
+
+  useEffect(() => {
+    if (isTimerRunning) {
+      const intervalId = setInterval(() => {
+        setTimeLeft((v) => v - 1)
+      }, 200) // TODO change temporary value back to 1000
+      return () => {
+        clearInterval(intervalId)
+      }
+    }
+  }, [isTimerRunning])
 
   const handleTimerRunningToggle = () => {
     setTimerRunning(!isTimerRunning)
@@ -16,7 +40,7 @@ const App = () => {
     <div className="m-16 grid grid-cols-2 grid-rows-2 max-w-md text-white font-bold bg-teal-500 border-8 border-teal-500 rounded-md drop-shadow-xl">
       <Timer
         className="row-span-2"
-        label="Session"
+        label={timerType}
         seconds={timeLeft}
         isTimerRunning={isTimerRunning}
         onStartStop={handleTimerRunningToggle}
