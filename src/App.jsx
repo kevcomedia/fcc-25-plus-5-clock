4f86ee92e1a0e92626fb62b582149a4323935f9e
Kev Comedia <kevcomedia@gmail.com>
2022-03-13 22:23:56 +0800

    Put components together

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Timer from './components/Timer'
+import TimerControl from './components/TimerControl'
+
 const App = () => {
-  return <p className="text-blue-700">25 + 5 clock</p>
+  return (
+    <div className="m-16 grid grid-cols-2 grid-rows-2 max-w-md text-white font-bold bg-teal-500 border-8 border-teal-500 rounded-md drop-shadow-xl">
+      <Timer className="row-span-2" label="Session" seconds={25 * 60} />
+      <TimerControl className="self-center" label="Break Length" value={5} />
+      <TimerControl className="self-center" label="Session Length" value={25} />
+    </div>
+  )
 }
 
 export default App
