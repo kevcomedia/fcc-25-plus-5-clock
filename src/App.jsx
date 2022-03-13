4e69c05e44d5e43fc5eaacdc67ff401f03a5c938
Kev Comedia <kevcomedia@gmail.com>
2022-03-13 22:27:45 +0800

    Add timer control ids

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,18 @@ const App = () => {
   return (
     <div className="m-16 grid grid-cols-2 grid-rows-2 max-w-md text-white font-bold bg-teal-500 border-8 border-teal-500 rounded-md drop-shadow-xl">
       <Timer className="row-span-2" label="Session" seconds={25 * 60} />
-      <TimerControl className="self-center" label="Break Length" value={5} />
-      <TimerControl className="self-center" label="Session Length" value={25} />
+      <TimerControl
+        className="self-center"
+        name="break"
+        label="Break Length"
+        value={5}
+      />
+      <TimerControl
+        className="self-center"
+        name="session"
+        label="Session Length"
+        value={25}
+      />
     </div>
   )
 }
--- src/components/TimerControl.jsx
+++ src/components/TimerControl.jsx
@@ -1,13 +1,21 @@
 import Button from './Button'
 
-const TimerControl = ({ className, label, value }) => {
+const TimerControl = ({ className, name, label, value }) => {
   return (
     <div className={className}>
-      <p className="text-center">{label}</p>
+      <p className="text-center" id={`${name}-label`}>
+        {label}
+      </p>
       <div className="flex justify-center items-center">
-        <Button className="mr-4 p-2">-</Button>
-        <p className="mr-4 px-2">{value}</p>
-        <Button className="p-2">+</Button>
+        <Button className="mr-4 p-2" id={`${name}-decrement`}>
+          -
+        </Button>
+        <p className="mr-4 px-2" id={`${name}-length`}>
+          {value}
+        </p>
+        <Button className="p-2" id={`${name}-increment`}>
+          +
+        </Button>
       </div>
     </div>
   )
